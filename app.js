880268488eed02bd989a2a9a85dd70ec758e2542
jeimer3184@gmail.com <jeimer3184@gmail.com>
2016-05-26 03:48:03 -0500

    adding app.js

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 (function() {
   var app = angular.module('gemStore', ['store-directives']);
 
-  app.controller('StoreController', [ '$http', function($http){
-	var store = this;
-
-	store.products = [];
-
-    $http.get('products.json').success(function(data){
-    	store.products = data;
-    });
-  }]);
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
 
   app.controller('ReviewController', function() {
     this.review = {};
@@ -20,4 +14,50 @@
       this.review = {};
     };
   });
+
+  
+
+  var gems = [{
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        "dodecahedron-01-full.jpg",
+        "dodecahedron-01-full.jpg",
+        "dodecahedron-01-full.jpg"
+      ],
+      reviews: []
+    }, {
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        "pentagonal-gem-01-full.jpg",
+        "pentagonal-gem-01-full.jpg",
+        "pentagonal-gem-01-full.jpg",
+      ],
+      reviews: []
+    }, {
+      name: 'Zircon',
+      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      shine: 70,
+      price: 1100,
+      rarity: 2,
+      color: '#000',
+      faces: 6,
+      images: [
+        "pentagonal-gem-01-full.jpg",
+        "pentagonal-gem-01-full.jpg",
+        "pentagonal-gem-01-full.jpg"
+      ],
+      reviews: []
+    }];
 })();
