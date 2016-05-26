0352d8c33783e0c237e4a6be98ad6ba046ae4f4a
jeimer3184@gmail.com <jeimer3184@gmail.com>
2016-05-26 02:56:59 -0500

    Pushing major revision

--- app.js
+++ app.js
@@ -1,88 +1,141 @@
-(function(){
-	var app = angular.module('store',[ ]);
-	
-	app.controller('StoreController', function(){
-		this.products = gems;
-	});
-	app.controller('PanelController', function(){
-		this.tab = 1;
+(function() {
+  var app = angular.module('gemStore', []);
 
-		this.selectTab = function(setTab){
-			this.tab = setTab;
-		};
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  });
 
-		this.isSelected = function(checkTab){
-			return this.tab === checkTab;
-		};
-	});
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
 
-	app.controller('ReviewController', function(){
-		this.review = {};
-		this.addReview = function(product){
-			product.reviews.push(this.review);
-			this.review = {};
-		};
-	});
+  app.directive('productTabs', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controller: function() {
+        this.tab = 1;
 
-	var gems = [
-		{
-			name: 'Dodechahedron',
-			price: 2,
-			description: '. . .',
-			reviews: 'None Yet',
-			specifications: 'None Yet',
-			images: [
-				{
-					full: 'dodecahedron-01-full.jpg',
-					thumb: 'dodecahedron-01-thumb.jpg'
-				},
-				{
-					full: 'dodecahedron-02-full.jpg',
-					thumb: 'dodecahedron-02-thumb.jpg'
-				}
-			],
-			reviews: [
-				{
-					stars: 5,
-					body: 'I love this product!',
-					author: 'joe@thomas.com'
-				},
-				{
-					stars: 1,
-					body: 'This product sucks',
-					author: 'tim@hater.com'
-				}
-			]
-		},
-		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: '. . .',
-			reviews: 'None Yet',
-			specifications: 'None Yet',
-			images: [
-				{
-					full: 'pentagonal-gem-01-full.jpg',
-					thumb: 'pentagonal-gem-01-thumb.jpg'
-				},
-				{
-					full: 'pentagonal-gem-02-full.jpg',
-					thumb: 'pentagonal-gem-02-thumb.jpg'
-				}
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
 
-			],
-			reviews: [
-				{
-					stars: 5,
-					body: 'I love this product!',
-					author: 'joe@thomas.com'
-				},
-				{
-					stars: 1,
-					body: 'This product sucks',
-					author: 'tim@hater.com'
-				}
-			]
-		}
-	];
+        this.setTab = function(setTab) {
+          this.tab = setTab;
+        };
+      },
+      controllerAs: 'tab'
+    };
+  });
+
+  
+
+  app.controller("ReviewController", function(){
+
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+
+  });
+
+  app.directive("productDescription", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-description.html"
+    };
+  });
+
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
+    return {
+      restrict:"A",
+      templateUrl: "product-specs.html"
+    };
+  });
+
+  var gems = [
+    {
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        "dodecahedron-01-full.jpg",
+        "dodecahedron-01-full.jpg",
+        "dodecahedron-01-full.jpg"
+      ],
+      reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org"
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org"
+      }]
+    }, {
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        "dodecahedron-01-full.jpg",
+        "dodecahedron-01-full.jpg",
+        "dodecahedron-01-full.jpg"
+      ],
+      reviews: [{
+        stars: 3,
+        body: "I think this gem was just OK, could honestly use more shine, IMO.",
+        author: "JimmyDean@example.org"
+      }, {
+        stars: 4,
+        body: "Any gem with 12 faces is for me!",
+        author: "gemsRock@example.org"
+      }]
+      }, {
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "pentagonal-gem-01-full.jpg",
+          "pentagonal-gem-01-full.jpg",
+          "pentagonal-gem-01-full.jpg"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org"
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org"
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org"
+        }]
+    }
+  ];
 })();
